475ae353270b599f2e7f3205ca4506699038b6ab
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-10-10 21:15:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('Loaded!');
 console.log('Hi there!')
 var trial = document.getElementById('content');
 trial.innerHTML="PotterHead Aboard? You're in the right place! Mount your broomstick for a magical journey.";
-var image = document.getElementById('dino');
+/*var image = document.getElementById('dino');
 image.onclick=function()
 {
     image.style.marginLeft='100px';
-}
+}*/
 var img = document.getElementById('dino')
 var marginleft=0;
 function moveright()
