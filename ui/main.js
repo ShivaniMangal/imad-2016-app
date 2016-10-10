55a28c8a7541f7a0f349d47728c2857210187b93
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-10-10 20:45:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 console.log('Hi there!')
 var trial = document.getElementById('content');
 trial.innerHTML="Do I sense a PotterHead? You're in the right place! Mount your brooms for a magical journey!";
-var dino = document.getElementById('dino');
-dino.onclick=function()
+var image = document.getElementById('dino');
+image.onclick=function()
 {
-    dino.style.MarginLeft='100px';
-}
+    image.style.MarginLeft='100px';
+};
