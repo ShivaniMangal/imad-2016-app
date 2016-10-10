6f6bbb667da23a77e5279e5f8585524b617c43f5
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-10-10 18:19:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 console.log('Hi there!')
 var trial = document.getElementById('content');
-trial.innerHTML="Potter-heads, fly right in";
+trial.innerHTML="POTTER HEAD? You're in the right place! Mount your brooms for a magical journey!";
