c5e773c75695b87a77e2f941f2a08a26b79e6566
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-10-10 15:01:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/Marauders-Map', function(req,res){
+app.get('/MM', function(req,res){
 	res.sendFile(path.join(_dirname, 'ui','Map.html'));
 });
 app.get('/Sorting-Hat', function(req,res)
