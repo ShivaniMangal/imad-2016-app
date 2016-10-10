ccf964a37270c036b6493c5b67dac743bf7532b6
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-10-10 20:58:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,18 @@
 console.log('Loaded!');
 console.log('Hi there!')
 var trial = document.getElementById('content');
-trial.innerHTML="Do I sense a PotterHead? You're in the right place! Mount your brooms for a magical journey!";
+trial.innerHTML="Do I sense a PotterHead? You're in the right place! Mount your broomstick for a magical journey!";
 var image = document.getElementById('dino');
 image.onclick=function()
 {
     image.style.marginLeft='100px';
 };
+function moveleft()
+{
+    marginright = marginright+10;
+    image.style.marginright = marginright+'px';
+}
+image.onclick =function()
+{
+    var moving_time =  setinterval(moveleft,100);
+}
