7c012befb44c3d327a35924007d3748d03e1f77c
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-10-10 23:05:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/Hogwarts-lake.png', function (req, res) {
+app.get('/ui/hp.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui',  'hp.png'));
 });
 
