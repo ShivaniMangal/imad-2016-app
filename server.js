d6ede968ec9f6d8467bbcd492d9bea6073a7e83b
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-11-26 12:27:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/style2.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'style2.css'));
 });
 
 
