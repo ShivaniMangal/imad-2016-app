f06ce9d26c7125912ae8499bc5c5d048e4307a15
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-11-26 12:23:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get('/prof', function(req,res)
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/style2.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 
 app.get('/ui/hermione.jpg', function(req,res){
     res.sendFile(path.join(__dirname,'ui','hermione.jpg'));
