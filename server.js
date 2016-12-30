d2ed98f2bf55a2f6ebb209776033d5a2585c518f
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-12-30 17:09:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,6 +60,43 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/bootstrap.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css'));
+});
+
+app.get('/ui/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.css'));
+});
+
+app.get('/ui/bootstrap-theme.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap-theme.css'));
+});
+
+app.get('/ui/bootstrap-theme.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap-theme.min.css'));
+});
+
+
+app.get('/ui/bootstrap-theme.css.map', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap-theme.css.map'));
+});
+
+app.get('/ui/bootstrap-theme.min.css.map', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/bootstrap.css.map', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css.map'));
+});
+
+app.get('/ui/bootrap.min.css.map', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css.map'));
+});
+
+app.get('/navig', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'navigator.html'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
