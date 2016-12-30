fc263d54118208ac09ab5a755a7bf3f3ce729dd2
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-12-30 22:35:36 +0530

    Create comments.js

---
+++ ui/comments.js
@@ -0,0 +1,32 @@
+var submit =document.getElementById('submit_btn);
+submit.onclick = function()
+{
+//Creating a request obj
+var request = new XMLHttpRequest();
+//Capture and store the response in a variable
+request.onreadystatechange=function()
+{
+if(request.readystate==XMLHttpRequest.DONE)
+{//Action
+if(request.status==200)
+{
+//Capture a list of comments and render it as a list
+var names=request.resoponseText;
+names=JSON.parse(names);//convets string to JSON obj
+var list=' ';
+for(var i=0;i<names.length;i++)
+{
+list+='<li>+names[i]+'</li>;
+}
+var ul = document.getElementById('nameslist);
+ul.innerHTML=list;
+}
+}
+//Not done
+};
+//Make the request
+var nameInput=document.getElementById('name);
+var name= nameInput.value;
+request.open('GET','http://shivanimangal.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+};
