04d620ff57a81eaa709e6a386f7c290785f7dbaa
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-12-30 23:21:21 +0530

    [imad-console] Updates ui/comments.js

--- ui/comments.js
+++ ui/comments.js
@@ -27,6 +27,6 @@ ul.innerHTML=list;
 //Make the request
 var nameInput=document.getElementById('name');
 var name= nameInput.value;
-request.open('GET','http://shivanimangal.imad.hasura-app.io/submit-name?name=' +name, true);
+request.open('GET','http://shivanimangal.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
 };
