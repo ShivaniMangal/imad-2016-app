a60be613c31c2f9bb05dab2b311506041fbefdc8
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-12-31 17:50:58 +0530

    [imad-console] Updates ui/comments.js

--- ui/comments.js
+++ ui/comments.js
@@ -8,16 +8,16 @@ var REQ = new XMLHttpRequest();
 //Capture and store the response in a variable
 REQ.onreadystatechange = function()
 {
-if(REQ.readystate == XMLHttpRequest.DONE)
+if(REQ.readystate === XMLHttpRequest.DONE)
 {//Action
-if(REQ.status == 200 || REQ.readystate==4)
+if(REQ.status === 200 )
 {
 //Capture a list of comments and render it as a list
 var names=REQ.responseText;
-names=JSON.parse(names);//convets string to JSON obj i.e array
+var obj=JSON.parse(names);//convets string to JSON obj i.e array
 var list = '';
-for(var i=0; i< names.length; i++){
-list += '<li>' + names[i] + '</li>';
+for(var i=0; i< obj.length; i++){
+list += '<li>' + obj[i] + '</li>';
 }
 document.getElementById('work').innerHTML=list;
 //var ul = document.getElementById('work');
