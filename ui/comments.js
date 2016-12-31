5bd1cda1ce75a7ce17393d0757d031d0cbe2a016
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-12-31 14:17:02 +0530

    [imad-console] Updates ui/comments.js

--- ui/comments.js
+++ ui/comments.js
@@ -14,7 +14,7 @@ if(request.status === 200)
 //Capture a list of comments and render it as a list
 var names=request.responseText;
 names=JSON.parse(names);//convets string to JSON obj ie array
-var list = '';
+var list = "";
 for(var i=0; i< names.length; i++)
 {
 list += '<li>' + names[i] + '</li>';
