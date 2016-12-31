806dc9d3384b1253955fd4f39c329042d9171f29
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-12-31 12:03:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,10 +52,6 @@ app.get('/submit-name',function(req,res)
     res.send(JSON.stringify(names));
 });
 
-app.post("/submit-name", function (request, response) {
-names.push(request.query.NAME);
-  response.sendStatus(200);
-});
 
 app.get('/ui/hermione.jpg', function(req,res){
     res.sendFile(path.join(__dirname,'ui','hermione.jpg'));
