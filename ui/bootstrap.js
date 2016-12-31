8249df0443dc590043f6a7188ce497af9ee95a0d
ShivaniMangal <shivanimangaleswaran@gmail.com>
2016-12-31 10:05:43 +0530

    [imad-console] Updates ui/bootstrap.js

--- ui/bootstrap.js
+++ ui/bootstrap.js
@@ -3,6 +3,8 @@
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under the MIT license
  */
+ 
+
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
